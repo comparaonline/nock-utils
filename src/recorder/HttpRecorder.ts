a4be7cd566b8db39dd018bd344a79568931a9404
Esteban Martini <emartini@comparaonline.com>
2025-01-17 12:08:23 -0300

    chore(Package): upgrade all packages + tests

--- src/recorder/HttpRecorder.ts
+++ src/recorder/HttpRecorder.ts
@@ -1,38 +1,38 @@
-import * as nock from 'nock';
-import * as fs from 'fs';
-import * as jsonfile from 'jsonfile';
-import { inspect } from 'util';
+import * as nock from 'nock'
+import * as fs from 'fs'
+import * as jsonfile from 'jsonfile'
+import { inspect } from 'util'
 
 export interface RecordOptions {
-  dont_print: boolean;
-  output_objects: boolean;
-  use_separator: boolean;
-  enable_reqheaders_recording: boolean;
+  dont_print: boolean
+  output_objects: boolean
+  use_separator: boolean
+  enable_reqheaders_recording: boolean
 }
 
 const WRITE_OPTIONS = {
   spaces: 2,
   EOL: '\r\n',
   flag: 'a'
-};
+}
 
 const DEFAULT_RECORD_OPTIONS: RecordOptions = {
   dont_print: true,
   output_objects: true,
   use_separator: false,
   enable_reqheaders_recording: false
-};
+}
 
 export class HttpRecorder {
-  private path: string;
+  private readonly path: string
 
   /**
    * Constructs an HttpRecorder with the path of the cassette to load
    * or save to.
    * @param path The path to the cassette
    */
-  constructor(path: string) {
-    this.path = path;
+  constructor (path: string) {
+    this.path = path
   }
 
   /**
@@ -42,15 +42,15 @@ export class HttpRecorder {
    * Important: Make sure to call @method stop after.
    * @param options Optional options to change the default ones
    */
-  start(options: Partial<RecordOptions> = {}) {
-    const isLoaded = this.isCassetteLoaded();
-    const recordOptions = { ...DEFAULT_RECORD_OPTIONS, ...options };
+  start (options: Partial<RecordOptions> = {}): void {
+    const isLoaded = this.isCassetteLoaded()
+    const recordOptions = { ...DEFAULT_RECORD_OPTIONS, ...options }
 
     if (isLoaded) {
-      nock.disableNetConnect();
-      this.play();
+      nock.disableNetConnect()
+      this.play()
     } else {
-      this.record(recordOptions);
+      this.record(recordOptions)
     }
   }
 
@@ -63,8 +63,8 @@ export class HttpRecorder {
    * isn't and netConnect was enabled it will be retrieved from the server.
    * @param host Optional The host or IP that will be enabled (can be a regexp)
    */
-  enableNetConnect(host?: string|RegExp) {
-    nock.enableNetConnect(host);
+  enableNetConnect (host?: string | RegExp): void {
+    nock.enableNetConnect(host)
   }
 
   /**
@@ -72,49 +72,49 @@ export class HttpRecorder {
    * It will write to the cassette file if it does not exist and
    * restore nock to avoid intercepting future http requests.
    */
-  stop(checkAllRan = false) {
-    const isLoaded = this.isCassetteLoaded();
+  stop (checkAllRan = false): void {
+    const isLoaded = this.isCassetteLoaded()
 
     if (!isLoaded) {
-      this.writeToFile();
+      this.writeToFile()
     }
     try {
       if (checkAllRan && !nock.isDone()) {
-        const pending = inspect(nock.pendingMocks(), false, 3, false);
+        const pending = inspect(nock.pendingMocks(), false, 3, false)
         throw new Error(`The following mocks didn't run: ${pending}`)
       }
     } finally {
-      nock.restore();
-      nock.cleanAll();
-      this.enableNetConnect();
+      nock.restore()
+      nock.cleanAll()
+      this.enableNetConnect()
     }
   }
 
-  private record(options: RecordOptions) {
-    nock.recorder.rec(options);
+  private record (options: RecordOptions): void {
+    nock.recorder.rec(options)
   }
 
-  private play() {
-    if (!nock.isActive()) nock.activate();
-    nock.load(this.path);
+  private play (): void {
+    if (!nock.isActive()) nock.activate()
+    nock.load(this.path)
   }
 
-  private writeToFile() {
-    const calls = nock.recorder.play();
+  private writeToFile (): void {
+    const calls = nock.recorder.play()
 
-    jsonfile.writeFileSync(this.path, calls, WRITE_OPTIONS);
-    nock.recorder.clear();
+    jsonfile.writeFileSync(this.path, calls, WRITE_OPTIONS)
+    nock.recorder.clear()
   }
 
-  isCassetteLoaded(): boolean {
-    return fs.existsSync(this.path);
+  isCassetteLoaded (): boolean {
+    return fs.existsSync(this.path)
   }
 
-  isActive(): boolean {
-    return nock.isActive();
+  isActive (): boolean {
+    return nock.isActive()
   }
 
-  getPath(): string {
-    return this.path;
+  getPath (): string {
+    return this.path
   }
 }
